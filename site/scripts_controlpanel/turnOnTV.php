2399e8c01b779f4a5f67470dd56899d90b3a0195
speedyswimmer1000 <speedyswimmer1000@gmail.com>
2017-10-14 13:21:11 -0400

    asdf

--- site/scripts_controlpanel/turnOnTV.php
+++ site/scripts_controlpanel/turnOnTV.php
@@ -35,52 +35,49 @@
 	$formedArray = array("On" => "True");
 
 	$parsed_text =  json_encode($formedArray, JSON_UNESCAPED_SLASHES);*/
-	
+	/*
 	if (!$allValid){
 		$exceptions[] = "Not all criteria in the JSON were valid. See console for more info.";
-	}else{
+	}else{*/
 		// We're good to go! All these have been validated to be good JSON. 
-        $debug[] = "All parsed arguments were valid: " . $parsed_text;
-        $debug[] = "Slideshow options were: " . $options;
 
-		$url = "http://127.0.0.1:" . $xml_params->serverParams->displayServerPort;
+	$url = "http://127.0.0.1:" . $xml_params->serverParams->displayServerPort;
 
-        // Request that we set the slideshow properties
+    // Request that we set the slideshow properties
 
-        $requestArray = array();        
-        $optionRequest = xmlrpc_encode_request("turnOnTV" , array("On" => "True"));
+    $requestArray = array();        
+    $optionRequest = xmlrpc_encode_request("turnOnTV" , array("On" => "True"));
 
-        $requestArray[] = $optionRequest;
+    $requestArray[] = $optionRequest;
 
-        for ($x = 0; $x < count($requestArray); $x++ ){
-        	$request = $requestArray[$x];
-	        // Request that we query the database and get the files 
-			$context = stream_context_create(array('http' => array(
-			    'method' => "POST",
-			    'header' => "Content-Type: text/xml",
-			    'content' => $request
-			)));
+    for ($x = 0; $x < count($requestArray); $x++ ){
+    	$request = $requestArray[$x];
+        // Request that we query the database and get the files 
+		$context = stream_context_create(array('http' => array(
+		    'method' => "POST",
+		    'header' => "Content-Type: text/xml",
+		    'content' => $request
+		)));
 
-			try{
-				$file = file_get_contents($url, false, $context);
-			}catch (Exception $e){
-				$exceptions[] = "It appears that the display server isn't running.";
-				$retArray = array('exceptions' => $exceptions, 'debug' => $debug );
-				echo json_encode($retArray);
-				return;
-			}
-			$response = xmlrpc_decode($file);
-			try{	
-					$jsonArray = json_decode($response, true);
-					$errs = $jsonArray['exceptions'];
-					$debugs = $jsonArray['debug'];
-					$debug = array_merge($debug, $debugs);
-					$exceptions = array_merge($exceptions, $errs);
-			}catch (Exception $e){
-				$exceptions[] = $response;
-			}
+		try{
+			$file = file_get_contents($url, false, $context);
+		}catch (Exception $e){
+			$exceptions[] = "It appears that the display server isn't running.";
+			$retArray = array('exceptions' => $exceptions, 'debug' => $debug );
+			echo json_encode($retArray);
+			return;
+		}
+		$response = xmlrpc_decode($file);
+		try{	
+				$jsonArray = json_decode($response, true);
+				$errs = $jsonArray['exceptions'];
+				$debugs = $jsonArray['debug'];
+				$debug = array_merge($debug, $debugs);
+				$exceptions = array_merge($exceptions, $errs);
+		}catch (Exception $e){
+			$exceptions[] = $response;
+		}
 
-	    }
 
 	}
 
