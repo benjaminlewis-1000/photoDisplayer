a61315fdab7843626a8e6dca81143ae1dc4b94cc
Ben <speedyswimmer1000@gmail.com>
2017-11-30 22:20:03 -0500

    Scoping of waiting for variables

--- site/scripts_controlpanel/schedulingLines.js
+++ site/scripts_controlpanel/schedulingLines.js
@@ -2,135 +2,113 @@
 
 function addScheduleLine(divOfFields, daySelected, timeStart, timeStop, showName){
 
+	// Wait for the definedSlideshows variable to be defined
+	waitForDefinedSlideshows(function(){
 	// If the show no longer exists, we will not make the line. 
-	var makeLine = false;
-	for (var i = 0; i < definedSlideshows.length; i++){	
-		if (showName == definedSlideshows[i]){
-			makeLine = true;
+		var makeLine = false;
+		for (var i = 0; i < definedSlideshows.length; i++){	
+			if (showName == definedSlideshows[i]){
+				makeLine = true;
+			}
 		}
-	}
-	if (makeLine == false && showName != null){
-		console.log('not making line')
-		return
-	}
-
-	// Get the appropriate variable and increment it.
-	var num = (document.getElementById('scheduleNumID').value - 1)+ 2;
-	document.getElementById('scheduleNumID').value = num;
-
-	console.debug('Div number is ' + num)
-
-	var mainDiv = document.getElementById(divOfFields);
-	var lineDiv = document.createElement('div');
-
-	mainDiv.appendChild(lineDiv);
-
-	// Day of week drop-down button
-	var span_day_of_week = document.createElement('span');
-	var select_day_of_week = document.createElement('select');
-	lineDiv.appendChild(span_day_of_week);
-	span_day_of_week.appendChild(select_day_of_week);
-
-	days = ["Every Day", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "On Given Day"];
-	for (var i = 0; i < days.length; i++){
-		select_day_of_week.options.add(new Option(days[i], days[i]));
-	}
-
-	if (daySelected != null && days.indexOf(daySelected) >= 0){
-		select_day_of_week.value = daySelected
-	}
-
-	lineDiv.className = "fieldParent";
-	select_day_of_week.className = "criteriaType";
-	lineDiv.id = 'scheduleBox' + num;
-	select_day_of_week.id = 'dayOfWeekBox' + num;
-	select_day_of_week.onchange = function(){
-		checkAllDefined(num)
-	}
-
-	// Put the "From" label on the time 
-	var startLabel = document.createElement('span')
-	startLabel.innerHTML="From:"
-	startLabel.className = "labelText"
-	lineDiv.appendChild(startLabel)
-
-	// Start time input field
-	var span_start_time = document.createElement('span');
-	var input_start_time = document.createElement('input');
-	lineDiv.appendChild(span_start_time);
-	span_start_time.appendChild(input_start_time);
-
-	input_start_time.className = "timeInputBox";
-	input_start_time.id = "showStartTime" + num;
-	input_start_time.setAttribute('type',"text");
-
-	if (timeStart != null){
-		input_start_time.value = standardizeTimeAMPM(timeStart);
-	}
-
-	input_start_time.onchange = function(){checkTime('start', num) };
-
-	// Put the "to" label in
-	var stopLabel = document.createElement('span')
-	stopLabel.innerHTML="To:"
-	stopLabel.className = "labelText"
-	lineDiv.appendChild(stopLabel)
-
-	// Stop time input field
-	var span_stop_time = document.createElement('span');
-	var input_stop_time = document.createElement('input');
-	lineDiv.appendChild(span_stop_time);
-	span_stop_time.appendChild(input_stop_time);
-
-	input_stop_time.className = "timeInputBox";
-	input_stop_time.id = "showStopTime" + num;
-	input_stop_time.setAttribute('type',"text");
-
-	if (timeStop != null){
-		input_stop_time.value = standardizeTimeAMPM(timeStop);
-	}
-
-	input_stop_time.onchange = function(){checkTime('stop', num) };
-	
-	// Show name field 
-	var span_show_name = document.createElement('span');
-	var select_show_name = document.createElement('select');
-	lineDiv.appendChild(span_show_name);
-	span_show_name.appendChild(select_show_name);
-
-	select_show_name.id = "showNameSelect" + num;
-	select_show_name.className = "dropdownOptions";
-	select_show_name.style.width=31 + "%"
-
-
-	// The value of 'definedSlideshows' is managed over in modalScript.php and updates dynamically
-	// every time we have a new or deleted slideshow. The theory is to have few calls to the database
-	// and then rebuild the list when necessary.
-	var i;
-    for(i = select_show_name.options.length - 1 ; i >= 0 ; i--)
-    {
-        select_show_name.remove(i);
-    }
-
-	for (var i = 0; i < definedSlideshows.length; i++){	
-		select_show_name.options.add(new Option(definedSlideshows[i], definedSlideshows[i]));
-		if (showName == definedSlideshows[i]){
-			select_show_name.value = showName
+		if (makeLine == false && showName != null){
+			showName = 'All'
+			//return
 		}
-	}
 
-	select_show_name.onchange = function(){
-		checkAllDefined(num)
-	}
+		// Get the appropriate variable and increment it.
+		var num = (document.getElementById('scheduleNumID').value - 1)+ 2;
+		document.getElementById('scheduleNumID').value = num;
 
-	select_show_name.onclick = function(){
+		console.debug('Div number is ' + num)
+
+		var mainDiv = document.getElementById(divOfFields);
+		var lineDiv = document.createElement('div');
+
+		mainDiv.appendChild(lineDiv);
+
+		// Day of week drop-down button
+		var span_day_of_week = document.createElement('span');
+		var select_day_of_week = document.createElement('select');
+		lineDiv.appendChild(span_day_of_week);
+		span_day_of_week.appendChild(select_day_of_week);
+
+		days = ["Every Day", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "On Given Day"];
+		for (var i = 0; i < days.length; i++){
+			select_day_of_week.options.add(new Option(days[i], days[i]));
+		}
+
+		if (daySelected != null && days.indexOf(daySelected) >= 0){
+			select_day_of_week.value = daySelected
+		}
+
+		lineDiv.className = "fieldParent";
+		select_day_of_week.className = "criteriaType";
+		lineDiv.id = 'scheduleBox' + num;
+		select_day_of_week.id = 'dayOfWeekBox' + num;
+		select_day_of_week.onchange = function(){
+			checkAllDefined(num)
+		}
+
+		// Put the "From" label on the time 
+		var startLabel = document.createElement('span')
+		startLabel.innerHTML="From:"
+		startLabel.className = "labelText"
+		lineDiv.appendChild(startLabel)
+
+		// Start time input field
+		var span_start_time = document.createElement('span');
+		var input_start_time = document.createElement('input');
+		lineDiv.appendChild(span_start_time);
+		span_start_time.appendChild(input_start_time);
+
+		input_start_time.className = "timeInputBox";
+		input_start_time.id = "showStartTime" + num;
+		input_start_time.setAttribute('type',"text");
+
+		if (timeStart != null){
+			input_start_time.value = standardizeTimeAMPM(timeStart);
+		}
+
+		input_start_time.onchange = function(){checkTime('start', num) };
+
+		// Put the "to" label in
+		var stopLabel = document.createElement('span')
+		stopLabel.innerHTML="To:"
+		stopLabel.className = "labelText"
+		lineDiv.appendChild(stopLabel)
+
+		// Stop time input field
+		var span_stop_time = document.createElement('span');
+		var input_stop_time = document.createElement('input');
+		lineDiv.appendChild(span_stop_time);
+		span_stop_time.appendChild(input_stop_time);
+
+		input_stop_time.className = "timeInputBox";
+		input_stop_time.id = "showStopTime" + num;
+		input_stop_time.setAttribute('type',"text");
+
+		if (timeStop != null){
+			input_stop_time.value = standardizeTimeAMPM(timeStop);
+		}
+
+		input_stop_time.onchange = function(){checkTime('stop', num) };
+		
+		// Show name field 
+		var span_show_name = document.createElement('span');
+		var select_show_name = document.createElement('select');
+		lineDiv.appendChild(span_show_name);
+		span_show_name.appendChild(select_show_name);
+
+		select_show_name.id = "showNameSelect" + num;
+		select_show_name.className = "dropdownOptions";
+		select_show_name.style.width=31 + "%"
+
+
 		// The value of 'definedSlideshows' is managed over in modalScript.php and updates dynamically
 		// every time we have a new or deleted slideshow. The theory is to have few calls to the database
 		// and then rebuild the list when necessary.
-
-		console.log('TODO: This should only update if the slideshow array has changed...')
 		var i;
-		current_val = select_show_name.value;
 	    for(i = select_show_name.options.length - 1 ; i >= 0 ; i--)
 	    {
 	        select_show_name.remove(i);
@@ -138,43 +116,68 @@ function addScheduleLine(divOfFields, daySelected, timeStart, timeStop, showName
 
 		for (var i = 0; i < definedSlideshows.length; i++){	
 			select_show_name.options.add(new Option(definedSlideshows[i], definedSlideshows[i]));
-			console.log(current_val + "  " + definedSlideshows[i])
-			if (current_val == definedSlideshows[i]){
-				select_show_name.value = current_val
+			if (showName == definedSlideshows[i]){
+				select_show_name.value = showName
 			}
 		}
-	}
 
-	// Remove button
+		select_show_name.onchange = function(){
+			checkAllDefined(num)
+		}
 
-	var removeSpan = document.createElement('span');
-	lineDiv.appendChild(removeSpan);
-	removeSpan.id = 'removeSubdivSlideshow' + num;
+		select_show_name.onclick = function(){
+			// The value of 'definedSlideshows' is managed over in modalScript.php and updates dynamically
+			// every time we have a new or deleted slideshow. The theory is to have few calls to the database
+			// and then rebuild the list when necessary.
 
-	var removeLineButton = document.createElement('button');
-	removeLineButton.id = 'button';
-	removeLineButton.innerText = "Remove";
+			console.log('TODO: This should only update if the slideshow array has changed...')
+			var i;
+			current_val = select_show_name.value;
+		    for(i = select_show_name.options.length - 1 ; i >= 0 ; i--)
+		    {
+		        select_show_name.remove(i);
+		    }
 
-	removeLineButton.className = "fieldChild";
-	removeSpan.appendChild(removeLineButton);
+			for (var i = 0; i < definedSlideshows.length; i++){	
+				select_show_name.options.add(new Option(definedSlideshows[i], definedSlideshows[i]));
+				console.log(current_val + "  " + definedSlideshows[i])
+				if (current_val == definedSlideshows[i]){
+					select_show_name.value = current_val
+				}
+			}
+		}
 
-	removeLineButton.onclick = function() { 
-		removeElement(lineDiv.id);
-		checkAllDefined(num)
-	};
+		// Remove button
 
-	// Grow the accordion if it is in display mode. Take the name of the accordion div (which
-	// holds the actual options of this type), get the next sibling, and set it to active.
-	var acc = document.getElementById("slideshowSchedulingAccordion");
-    var panel = acc.nextElementSibling;
+		var removeSpan = document.createElement('span');
+		lineDiv.appendChild(removeSpan);
+		removeSpan.id = 'removeSubdivSlideshow' + num;
 
-    if (acc.className == "accordion"){  
-    // Toggle the criteria div on; it's clearly advantageous to see what you're adding.
-    	acc.classList.toggle("active");
-    }
-    if (acc.className == "accordion active"){
-	    panel.style.maxHeight = panel.scrollHeight + "px";
-	}
+		var removeLineButton = document.createElement('button');
+		removeLineButton.id = 'button';
+		removeLineButton.innerText = "Remove";
+
+		removeLineButton.className = "fieldChild";
+		removeSpan.appendChild(removeLineButton);
+
+		removeLineButton.onclick = function() { 
+			removeElement(lineDiv.id);
+			checkAllDefined(num)
+		};
+
+		// Grow the accordion if it is in display mode. Take the name of the accordion div (which
+		// holds the actual options of this type), get the next sibling, and set it to active.
+		var acc = document.getElementById("slideshowSchedulingAccordion");
+	    var panel = acc.nextElementSibling;
+
+	    if (acc.className == "accordion"){  
+	    // Toggle the criteria div on; it's clearly advantageous to see what you're adding.
+	    	acc.classList.toggle("active");
+	    }
+	    if (acc.className == "accordion active"){
+		    panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	})
 }
 
 function removeElement(parentName){
@@ -351,3 +354,15 @@ function populateSlideshowTimes(){
 		}
 	})
 }
+
+
+function waitForDefinedSlideshows(callback){
+	if (typeof definedSlideshows !== 'undefined'){
+
+		callback()
+	}else{
+		console.log('waiting...')
+		setTimeout(function(){ waitForPersonNames(callback)} , 750) 
+	}
+}
+
