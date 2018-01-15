eec857a95cf33c7e2a4a3dccb097de4f5b89693c
Ben <speedyswimmer1000@gmail.com>
2018-01-15 14:38:22 -0500

    SSH Secure login

--- install/subscripts/sshEnable.sh
+++ install/subscripts/sshEnable.sh
@@ -2,10 +2,19 @@
 
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
-sudo perl -0777 -p -i -e 's|[# ]+?PermitRootLogin.*|PermitRootLogin yes|g' /etc/ssh/sshd_config
+sudo perl -0777 -p -i -e 's|[# ]+?PermitRootLogin.*|PermitRootLogin no|g' /etc/ssh/sshd_config
+sudo perl -0777 -p -i -e 's|[# ]+?UsePAM.*|UsePAM no|g' /etc/ssh/sshd_config
+sudo perl -0777 -p -i -e 's|[# ]+?PasswordAuthentication.*|PasswordAuthentication no|g' /etc/ssh/sshd_config
 
 sudo service ssh restart
 
 # Put service ssh start before the exit 0 in rc.local
 sudo perl -p -i -e 's|^exit 0|service ssh start\n\nexit 0|g' /etc/rc.local
 
+mkdir ~/.ssh
+sudo chown $USER ~/.ssh
+sudo chmod 700 ~/.ssh
+sudo cp lewis_family_id_rsa.pub ~/.ssh
+
+cat ~/.ssh/lewis_family_id_rsa.pub >> ~/.ssh/authorized_keys
+sudo chmod 644 ~/.ssh/authorized_keys
