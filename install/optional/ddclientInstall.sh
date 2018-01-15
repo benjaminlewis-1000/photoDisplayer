b6d84c8c0a153beca4b29662e037732b5aac93c3
Ben <speedyswimmer1000@gmail.com>
2018-01-15 13:45:18 -0500

    Dynamic DNS Updater

---
+++ install/optional/ddclientInstall.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# sudo apt-get install ddclient -y
+
+USERNAME=benjaminlewis
+PASSWORD=throwawaydns
+
+read -r -d '' DD_CONFIG << EOM
+# ddclient configuration for Dynu \n
+# \n
+# Configuration file from http://www.dynu.com/DynamicDNS/IPUpdateClient/DDClient \n
+#  \n
+# /etc/ddclient.conf \n
+daemon=60   #  Check every 60 seconds \n
+syslog=yes \n
+mail=root \n
+mail-failure=root\n
+pid=/var/run/ddclient.pid\n\
+\n
+use=web, web=checkip.dynu.com/, web-skip='IP Address'\n
+server=api.dynu.com\n
+protocol=dyndns2\n
+login=$USERNAME\n
+password=$PASSWORD\n
+lewisfamily.ddnsfree.com\n
+EOM
+
+rm ddclient.conf
+echo -e $DD_CONFIG > ddclient.conf
+
+sudo cp ddclient.conf /etc/ddclient.conf 
+
+sudo chown $USER /etc/ddclient.conf
+
+rm ddclient.conf
+
+/usr/sbin/ddclient -daemon 300 -syslog
